a473c4ade6d18bb3fc29d93ed8eb507766d6dc58
Daniel Santa Cruz <dstcruz@gmail.com>
2015-11-10 11:50:14 -0700

    Creating package for pt

---
+++ packages/pt/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= $env:chocolateyPackageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/monochromegane/the_platinum_searcher/releases/download/v1.7.9/pt_windows_386.zip'
+$url64      = 'https://github.com/monochromegane/the_platinum_searcher/releases/download/v1.7.9/pt_windows_amd64.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
