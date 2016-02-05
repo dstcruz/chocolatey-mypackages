151a49b3413ae4d3aacecb9f18b1d8fedfa3c9e1
Daniel Santa Cruz <dstcruz@gmail.com>
2016-02-05 11:08:46 -0700

    For pt, use nuspec file to drive version to download

--- packages/pt/tools/chocolateyinstall.ps1
+++ packages/pt/tools/chocolateyinstall.ps1
@@ -2,9 +2,8 @@
 
 $packageName = $env:chocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version     = "2.1.0"
-$url         = ("https://github.com/monochromegane/the_platinum_searcher/releases/download/v{0}/pt_windows_386.zip" -f $version)
-$url64       = ("https://github.com/monochromegane/the_platinum_searcher/releases/download/v{0}/pt_windows_amd64.zip" -f $version)
+$url         = ("https://github.com/monochromegane/the_platinum_searcher/releases/download/v{0}/pt_windows_386.zip" -f $env:chocolateyPackageVersion)
+$url64       = ("https://github.com/monochromegane/the_platinum_searcher/releases/download/v{0}/pt_windows_amd64.zip" -f $env:chocolateyPackageVersion)
 
 $packageArgs = @{
   packageName   = $packageName
