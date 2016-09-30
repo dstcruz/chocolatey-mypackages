b3595c6be42130b5bdfca32d6946ea91c29fa798
Daniel Santa Cruz <dstcruz@gmail.com>
2016-09-30 09:57:06 -0600

    Adding new package ripgrep v0.2.1

---
+++ packages/ripgrep/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url         = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/ripgrep-{0}-i686-pc-windows-msvc.zip" -f $env:chocolateyPackageVersion)
+$url64       = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/ripgrep-{0}-x86_64-pc-windows-msvc.zip" -f $env:chocolateyPackageVersion)
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+}
+
+Install-ChocolateyZipPackage @packageArgs
