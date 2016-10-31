1f723abceb78f8ef2e4763cc68dba81399fd0572
Daniel Santa Cruz <dstcruz@gmail.com>
2016-10-31 15:28:56 -0600

    Update to ripgrep 0.2.5
    Also, added extra metadata for ripgrep

--- packages/ripgrep/tools/chocolateyinstall.ps1
+++ packages/ripgrep/tools/chocolateyinstall.ps1
@@ -7,11 +7,15 @@ $url         = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/rip
 $url64       = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/ripgrep-{0}-x86_64-pc-windows-msvc.zip" -f $env:chocolateyPackageVersion)
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  url64bit      = $url64
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
+  checksum       = "29B2CDFE3E06B79E216E51705BE15CD62133456F"
+  checksumType   = "sha1"
+  url64bit       = $url64
+  checksum64     = "854273468BA6C02FD06DFB02C81E4E3A30F08E24"
+  checksumType64 = "sha1"
 }
 
 Install-ChocolateyZipPackage @packageArgs
