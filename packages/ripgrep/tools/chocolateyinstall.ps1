f25089b1c55efd130186a3dc3fe56ce9eb02c1e2
Daniel Santa Cruz <dstcruz@gmail.com>
2016-11-18 14:27:54 -0700

    Added ripgrep dependency on vcredist2015
    This dependency requirement should go away once https://github.com/rust-lang/rust/pull/37545
    is implemented.
    
    I'm creating version 0.2.9.1, which points to upstream 0.2.9 as a
    package only version to get this dependency in.

--- packages/ripgrep/tools/chocolateyinstall.ps1
+++ packages/ripgrep/tools/chocolateyinstall.ps1
@@ -1,10 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$version     = "0.2.9"
 $packageName = $env:chocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url         = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/ripgrep-{0}-i686-pc-windows-msvc.zip" -f $env:chocolateyPackageVersion)
-$url64       = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/ripgrep-{0}-x86_64-pc-windows-msvc.zip" -f $env:chocolateyPackageVersion)
+$url         = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/ripgrep-{0}-i686-pc-windows-msvc.zip" -f $version)
+$url64       = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/ripgrep-{0}-x86_64-pc-windows-msvc.zip" -f $version)
 
 $packageArgs = @{
   packageName    = $packageName
