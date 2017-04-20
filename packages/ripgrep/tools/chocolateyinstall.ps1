1fccd0292a7807ae7dc46ff02feed2fbd6354931
Daniel Santa Cruz <dstcruz@gmail.com>
2017-04-20 11:46:28 -0600

    bump ripgrep to 0.5.1

--- packages/ripgrep/tools/chocolateyinstall.ps1
+++ packages/ripgrep/tools/chocolateyinstall.ps1
@@ -8,15 +8,15 @@ $url         = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/rip
 $url64       = ("https://github.com/BurntSushi/ripgrep/releases/download/{0}/ripgrep-{0}-x86_64-pc-windows-msvc.zip" -f $version)
 
 $packageArgs = @{
-  packageName    = $packageName
-  unzipLocation  = $toolsDir
-  fileType       = 'exe'
-  url            = $url
-  checksum       = "13E05C4E6422E7E04A4F239B4617B1DEE7F38CC3"
-  checksumType   = "sha1"
-  url64bit       = $url64
-  checksum64     = "FBDE330AA8FCF81827B3DB9EF120DB47314D3373"
-  checksumType64 = "sha1"
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    checksum       = "90F88A33C8B92714C162B6E78284B84DF4D4306D"
+    checksumType   = "sha1"
+    url64bit       = $url64
+    checksum64     = "AADB931F2C6DC357DAF01ACD2B4B8CD897EE2D7D"
+    checksumType64 = "sha1"
 }
 
 Install-ChocolateyZipPackage @packageArgs
