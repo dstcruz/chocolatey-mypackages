b05fa90a0b3c6d8afe8e5cd1fb0bc07e907ffd60
Daniel Santa Cruz <dstcruz@gmail.com>
2018-05-30 10:02:53 -0600

    Adding a package for psc-package 0.3.2

---
+++ packages/psc-package/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$version     = $env:chocolateyPackageVersion
+$packageName = $env:chocolateyPackageName
+$url64       = ("https://github.com/purescript/psc-package/releases/download/v{0}/win64.tar.gz" -f $version)
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+    url64bit       = $url64
+    checksum64     = "bb6806fb63ccf648a1f4b8e0e946bc2324ab9e09"
+    checksumType64 = "sha1"
+}
+
+Install-ChocolateyZipPackage @packageArgs
+$File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.tar
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
