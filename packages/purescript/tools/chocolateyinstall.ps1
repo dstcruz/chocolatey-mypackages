6437dea0bc222d8c97b5427c813fbd80fb8f60da
Daniel Santa Cruz <dstcruz@gmail.com>
2018-06-29 08:57:15 -0600

    Bump purescript to 0.12.0
    Also, found this URL to be extremely handy when trying to get the
    full body of the release message:
    
    https://api.github.com/repos/purescript/purescript/releases

---
+++ packages/purescript/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$version     = $env:chocolateyPackageVersion
+$packageName = $env:chocolateyPackageName
+$url64       = ("https://github.com/purescript/purescript/releases/download/v{0}/win64.tar.gz" -f $version)
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+    url64bit       = $url64
+    checksum64     = "594c41bf9090bd01dfce32a0d29c6b700f06cf5b"
+    checksumType64 = "sha1"
+}
+
+Install-ChocolateyZipPackage @packageArgs
+$File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.tar
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
