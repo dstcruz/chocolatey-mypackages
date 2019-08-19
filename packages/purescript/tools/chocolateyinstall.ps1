68af1c425bc99b9d6455853c6c3d4c269161fe1e
Daniel Santa Cruz <dstcruz@gmail.com>
2019-08-19 09:22:21 -0600

    Bump purescript to v0.13.3

--- packages/purescript/tools/chocolateyinstall.ps1
+++ packages/purescript/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
     url64bit       = $url64
-    checksum64     = "2ee57096eee108228dba1591dbcac4221ed267a4"
+    checksum64     = "d21f01b7e138c6eb06193b3bd2f56eafc61620e6"
     checksumType64 = "sha1"
 }
 
