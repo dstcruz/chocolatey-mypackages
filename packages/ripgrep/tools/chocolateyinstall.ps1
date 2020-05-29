ac8b12bee1b416b913eb5b78f2f1314137e34b41
Daniel Santa Cruz <dstcruz@gmail.com>
2020-05-29 08:37:28 -0600

    Bump ripgrep to 12.1.1

--- packages/ripgrep/tools/chocolateyinstall.ps1
+++ packages/ripgrep/tools/chocolateyinstall.ps1
@@ -4,18 +4,18 @@ $version     = $env:chocolateyPackageVersion
 $packageName = $env:chocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url         = 'https://github.com/BurntSushi/ripgrep/releases/download/12.1.0/ripgrep-12.1.0-x86_64-pc-windows-msvc.zip'
-$url64       = 'https://github.com/BurntSushi/ripgrep/releases/download/12.1.0/ripgrep-12.1.0-i686-pc-windows-msvc.zip'
+$url         = 'https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep-12.1.1-x86_64-pc-windows-msvc.zip'
+$url64       = 'https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep-12.1.1-i686-pc-windows-msvc.zip'
 
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = $toolsDir
     fileType       = 'exe'
     url            = $url
-    checksum       = 'd448ce19f9bfe81a1014f5a46675ef7e26253191786ec86dcdd98da11392ae3f'
+    checksum       = 'a74d23c8c52a7cfddab029e3217cd7aad49e970ab3ee45fa72fa5f5536230088'
     checksumType   = 'sha256'
     url64bit       = $url64
-    checksum64     = '8dcca7ca3d498c7e36de50151f97b2c8dd2b7e216638479ecf6323624e50eeb9'
+    checksum64     = '0256451a21cf5cf88dcd1823ffd2acd5e89d6b5dc9c2c8ad1324fd0e1f9ff61e'
     checksumType64 = 'sha256'
 }
 
