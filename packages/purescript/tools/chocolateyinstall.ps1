1efb4eea54a590693b9cb00a540c45cc9db063b1
Daniel Santa Cruz <dsantacruz@spglobal.com>
2021-06-14 09:52:28 -0600

    Bump purescript to 0.14.2

--- packages/purescript/tools/chocolateyinstall.ps1
+++ packages/purescript/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
     packageName    = $Env:ChocolateyPackageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64          = 'https://github.com/purescript/purescript/releases/download/v0.14.1/win64.tar.gz'
-    checksum64     = 'c3fc893718d2c34c282f01cf49ee2f55d1dc4ed7eccacc985b1eef2b71604725' 
+    url64          = 'https://github.com/purescript/purescript/releases/download/v0.14.2/win64.tar.gz'
+    checksum64     = 'ef604c91771e99ff457eeff9fd69f5c0cb5b5380cd5280483c06e244588d1ab8' 
     checksumType64 = 'sha256'
 }
 
