6705f112ab61d1c2f311dce5d23636608b122279
Daniel Santa Cruz <dsantacruz@spglobal.com>
2021-07-06 12:30:57 -0600

    bump mdcat to 0.23.0

--- packages/mdcat/tools/chocolateyinstall.ps1
+++ packages/mdcat/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $version     = $env:chocolateyPackageVersion
 $packageName = $env:chocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url         = 'https://github.com/lunaryorn/mdcat/releases/download/mdcat-0.22.4/mdcat-0.22.4-x86_64-pc-windows-msvc.zip'
+$url         = 'https://github.com/lunaryorn/mdcat/releases/download/mdcat-0.23.0/mdcat-0.23.0-x86_64-pc-windows-msvc.zip'
 
 $packageArgs = @{
     packageName    = $packageName
@@ -12,7 +12,7 @@ $packageArgs = @{
     fileType       = 'exe'
     url            = $url
     checksumType   = 'sha256'
-    checksum       = 'ba883cc61393f1398388b5c378c663e5f48006e78c2ea0ca232195033f34c2b6'
+    checksum       = 'f851941924eb12fbb4f5186f96ca58ba3f44166e752b3cb12147f733f48777c5'
 }
 
 Install-ChocolateyZipPackage @packageArgs
